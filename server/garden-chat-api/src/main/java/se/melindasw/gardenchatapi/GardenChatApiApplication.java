0f2c847b628d0153e74c3727f38e7e4ed38a6bdb
Melinda <sandstrommelinda@gmail.com>
2020-09-22 22:35:30 +0200

    Basic server setup with spring boot

---
+++ server/garden-chat-api/src/main/java/se/melindasw/gardenchatapi/GardenChatApiApplication.java
@@ -0,0 +1,13 @@
+package se.melindasw.gardenchatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenChatApiApplication.class, args);
+	}
+
+}
---
+++ server/garden-chat-api/src/test/java/se/melindasw/gardenchatapi/GardenChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.melindasw.gardenchatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
