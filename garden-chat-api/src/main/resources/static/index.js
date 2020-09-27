3219ea9179e117aebde40a9280528fcca4e8e52e
Melinda <sandstrommelinda@gmail.com>
2020-09-27 16:26:19 +0200

    Ajax get messages request now working. Add annotation in controller for enabling cors requests

--- garden-chat-api/src/main/resources/static/index.js
+++ garden-chat-api/src/main/resources/static/index.js
@@ -1,7 +1,43 @@
 $(document).ready(function () {
-  // jQuery methods go here...
-  //   alert('Hello World!')
-  // get list of messages from server
-  // add list items with the messages, the newest on top.
-  //
+  const getAllMessages = () => {
+    getAllMessages()
+
+    $.get('http://localhost:8080/garden-chat-api/getallmessages')
+      .done((response) => {
+        renderMessages(response)
+      })
+      .fail((error) => {
+        console.log('failed', error)
+      })
+  }
+
+  const postMessage = (sender, message) => {
+    console.log('Posting message', sender, message)
+  }
+
+  $('form').submit((event) => {
+    event.preventDefault()
+    postMessage($('#namefield').val(), $('#messagefield').val())
+    $('#namefield').val() = ''
+    $('#messagefield').val() = ''
+    renderMessages(getAllMessages())
+  })
+
+  const renderMessages = (messages) => {
+    $.each(messages, (index, msg) => {
+      $('#messages').append(
+        `<li>
+        <img
+          id=${index % 2 === 0 ? 'leaf-left' : 'leaf-right'}
+          src=${index % 2 === 0 ? 'leaf-left.svg' : 'leaf-right.svg'}
+          alt='leaf'
+        />
+        <p>
+          <span id='sender'>${msg.sender + ':'}</span><br>
+          <span class='message'>${msg.message}</span>
+        </p>
+      </li>`
+      )
+    })
+  }
 })
